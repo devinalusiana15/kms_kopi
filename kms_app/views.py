ec2dd6173f529c4d493a9e4890b4d6950670b8d0
arianarmw <arianarahmawati0910@gmail.com>
2024-04-24 18:53:36 +0700

    FIX: Memperbaiki fungsi get answer

--- kms_app/views.py
+++ kms_app/views.py
@@ -4,6 +4,27 @@ import fitz
 from django.shortcuts import render
 from .forms import UploadFileForm
 from django.contrib import messages
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from .models import nlp_custom, nlp_default, document, merge_entities
+
+from django.http import HttpResponse
+
+def pos_tagging_and_extract_verbs(text):
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    pos_tags = pos_tag(tokens)
+    verbs = [word for word, pos in pos_tags if pos.startswith('VB') and word.lower() not in stop_words]
+    return verbs
+
+def pos_tagging_and_extract_nouns(text):
+    not_include = "coffee"
+    tokens = word_tokenize(text)
+    pos_tags = pos_tag(tokens)
+    nouns = [word for word, pos in pos_tags if pos.startswith('NN') and word != not_include]
+    return nouns
 
 def find_answer_type(question):
     question = question.lower().split()
@@ -11,9 +32,9 @@ def find_answer_type(question):
     entities = []
     if question[0] in format:
       if 'where' in question:
-          return ['LOC', 'GPE']
+          return ['LOC', 'GPE', 'CONTINENT']
       elif 'who' in question:
-          return ['NORP', 'PERSON']
+          return ['NORP', 'PERSON','NATIONALITY']
       elif 'when' in question:
           return ['DATE', 'TIME']
       elif 'what' in question:
@@ -21,10 +42,10 @@ def find_answer_type(question):
     else:
         return "Pertanyaan tidak valid"
 
-def find_answer(relevant_sentences, answer_types, entities):
+def find_answer(answer_types, entities):
     answer_types_mapping = {
-        'LOC': ['LOC','GPE'],
-        'PERSON': ['NORP', 'PERSON'],
+        'LOC': ['LOC','GPE', 'CONTINENT'],
+        'PERSON': ['NORP', 'PERSON','NATIONALITY'],
         'DATE': ['DATE', 'TIME'],
         'PRODUCT': ['PRODUCT', 'VARIETY', 'METHODS', 'BEVERAGE', 'QUANTITY']
     }
@@ -34,27 +55,137 @@ def find_answer(relevant_sentences, answer_types, entities):
                 return ent_text
     return "Tidak ada informasi yang ditemukan."
 
+def lemmatization(text):
+    doc = nlp_default(text)
+    filtered_tokens = [token.lemma_ for token in doc if token.lemma_ != token.is_stop]
+    return ' '.join(filtered_tokens)
+
+def create_inverted_index(documents):
+    inverted_index = {}
+    for doc_id, document in enumerate(documents):
+        tokens = document.lower().split()
+        for token in tokens:
+            if token not in inverted_index:
+                inverted_index[token] = []
+            if doc_id not in inverted_index[token]:
+                inverted_index[token].append(doc_id)
+    return inverted_index
+
+def retrieve_documents(documents, keywords=None, nouns=None):
+    result = []
+    rel_doc_set = set()
+    rel_doc = []
+    inv_index = create_inverted_index(documents)
+
+    # Jika tidak ada keyword dan noun
+    if keywords is None and nouns is None:
+        return result
+
+    # Jika ada keyword (verb)
+    if keywords is not None:
+        for keyword in keywords:
+            rel_doc_set.clear()
+            for token, index in inv_index.items():
+                if token == keyword:
+                    rel_doc_set.update(index)
+
+            # Konversi set ke list
+            rel_doc += [(i, documents[i]) for i in rel_doc_set]
+
+    # Jika ada noun
+    if nouns is not None:
+        for noun in nouns:
+            rel_doc_set.clear()
+            for token, index in inv_index.items():
+                if token == noun:
+                    rel_doc_set.update(index)
+
+            # Konversi set ke list
+            rel_doc += [(i, documents[i]) for i in rel_doc_set]
+
+    # format hasil pencarian
+    for doc_index, doc_content in rel_doc:
+        doc_entities = merge_entities(nlp_custom(doc_content))
+        entities = [(ent.text, ent.label_) for ent in doc_entities.ents]
+        result.append({
+            'document_index': doc_index,
+            'document_content': doc_content,
+            'entities': entities
+        })
+
+    return result
+
+def get_answer_new(question):
+    # Menghapus stop words
+    keywords = pos_tagging_and_extract_verbs(question)
+    keyword_noun = pos_tagging_and_extract_nouns(question)
+
+    response_text = f"Pertanyaan asli: {question}<br>Keywords: {keywords, keyword_noun}<br>"
+
+    answer_types = find_answer_type(question)
+    answer = None
+    search_result = retrieve_documents(document, keywords, keyword_noun)
+    
+    if search_result is not None:
+        for result in search_result:
+            doc_index = result['document_index']
+            doc_content = result['document_content']
+            entities = result['entities']
+            # # Menampilkan hasil pencarian
+            response_text += f"<br><br>Dokumen ke-{doc_index}: {doc_content}"
+
+            # # Mendapatkan entitas yang sesuai dengan answer_type
+            relevant_entities = [(ent_text, ent_label) for ent_text, ent_label in entities if ent_label in answer_types]
+
+            answer = find_answer(answer_types, relevant_entities)
+
+            if answer is not None:
+                response_text += f"Jawaban: {answer}"
+                break
+
+    if answer is None:
+        # Lemmatisasi kata kunci dan dokumen
+        lemmatized_keywords = [lemmatization(key) for key in keywords]
+        lemmatized_documents = [lemmatization(doc) for doc in document]
+
+        # Lakukan pencarian kembali
+        search_result_baru = retrieve_documents(lemmatized_documents, lemmatized_keywords, keyword_noun)
+        print(search_result_baru)
+        if search_result_baru is not None:
+            for result in search_result_baru:
+                doc_index = result['document_index']
+                doc_content = result['document_content']
+                entities = result['entities']
+                # Menampilkan hasil pencarian
+                response_text += f"<br><br>Dokumen ke-{doc_index}: {doc_content}"
+
+                # Mendapatkan entitas yang sesuai dengan answer_type
+                relevant_entities = [(ent_text, ent_label) for ent_text, ent_label in entities if ent_label in answer_types]
+
+                answer = find_answer(answer_types, relevant_entities)
+
+                if answer is not None:
+                    response_text += f"Jawaban: {answer}"
+                    break  # Keluar dari loop saat entitas cocok
+                else:
+                    response_text += f"<br>Jawaban tidak ditemukan."
+
+    context = {'response_text': response_text}
+    print(context)
+    return (answer)
+
 def home(request):
     if request.method == 'POST':
-        search_query = request.POST.get('default-search')
-        print("Nilai default-search:", search_query) # Buat pembuktian dulu
-        answer_type = find_answer_type(search_query)
-        if answer_type == "Pertanyaan tidak valid":
-            answer = "Pertanyaan tidak valid. Silahkan masukkan pertanyaan lagi."
-        else:
-            # relevant_sentences = []
-            # entities = [] 
-            # answer = find_answer(relevant_sentences, answer_type, entities)
-            # print(answer)
-            answer = "Success"
-        return render(request, 'Home.html', {'answer': answer})
+        search_query = request.POST.get('question')
+        print({"Pertanyaan: ", search_query})
+        answer_context = get_answer_new(search_query)
+        return render(request, 'Home.html', {'answer': answer_context})
     else:
         return render(request, 'Home.html')
     
 def articles(request):
     context_path = "kms_app/uploaded_files/coffee.pdf" 
     context = extract_text_from_pdf(context_path)
-    
     return render(request, 'pages/articles.html', {'context': context})
 
 def upload_file(request):
