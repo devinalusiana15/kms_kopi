0740350f5c1ea842c02a12775faf6528023db0b2
arianarmw <arianarahmawati0910@gmail.com>
2024-05-21 15:14:06 +0700

    FEAT: Menambahkan class LoginForm

--- kms_app/forms.py
+++ kms_app/forms.py
@@ -1,5 +1,9 @@
-# forms.py
 from django import forms
+from django.core.validators import MinLengthValidator
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput, validators=[MinLengthValidator(8)])
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
