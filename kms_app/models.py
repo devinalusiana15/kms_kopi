f874afa3b4b295694af3cf175dd14ffe5326be4f
arianarmw <arianarahmawati0910@gmail.com>
2024-05-25 19:16:54 +0700

    FIX: Menambahkan class untuk migrasi database

--- kms_app/models.py
+++ kms_app/models.py
@@ -1,6 +1,5 @@
 import nltk
 import spacy
-import fitz
 import json
 import subprocess
 import os.path
@@ -15,7 +14,7 @@ nltk.download('maxent_ne_chunker')
 nltk.download('words')
 nltk.download('stopwords')
 
-class uploader(models.Model):
+class Uploader(models.Model):
     uploader_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128, validators=[MinLengthValidator(8)])
@@ -23,10 +22,116 @@ class uploader(models.Model):
     def __str__(self):
         return self.username
 
-# Load model bahasa Inggris dari Spacy
+class Documents(models.Model):
+    document_id = models.AutoField(primary_key=True)  # ID unik untuk setiap dokumen
+    document_name = models.CharField(max_length=255)  # Nama atau judul dokumen
+    document_path = models.CharField(max_length=255)  # Isi dari dokumen tersebut
+
+    def __str__(self):
+        return self.document_name
+    
+class Terms(models.Model):
+    term_id = models.AutoField(primary_key=True)  # ID unik untuk setiap term
+    term = models.CharField(max_length=255)  # Term atau kata kunci yang muncul dalam dokumen
+
+    def __str__(self):
+        return self.term
+    
+class DocDetails(models.Model):
+    docdetail_id = models.AutoField(primary_key=True)  # ID unik untuk setiap dokumen
+    document = models.ForeignKey(Documents, on_delete=models.CASCADE)  # ID dokumen yang merujuk ke Tabel Dokumen
+    docdetail = models.CharField(max_length=255)  # Isi dari dokumen tersebut
+    position = models.IntegerField()
+
+    def __str__(self):
+        return self.docdetail
+
+class PostingLists(models.Model):
+    postlist_id = models.AutoField(primary_key=True)  # ID unik untuk setiap entri dalam posting list
+    term = models.ForeignKey(Terms, on_delete=models.CASCADE)  # ID term yang merujuk ke Tabel Term
+    docdetail = models.ForeignKey(DocDetails, on_delete=models.CASCADE)  # Frekuensi kemunculan term dalam dokumen tertentu
+
+    def __str__(self):
+        return f"{self.term} - {self.docdetail}"
+    
+class Refinements(models.Model):
+    refinement_id = models.AutoField(primary_key=True)
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+
+class TermLemmas(models.Model):
+    termlemma_id = models.AutoField(primary_key=True)  # ID unik untuk setiap term
+    termlemma = models.CharField(max_length=255)  # Term atau kata kunci yang muncul dalam dokumen
+
+    def __str__(self):
+        return self.termlemma
+    
+class PostingListLemmas(models.Model):
+    postlistlemma_id = models.AutoField(primary_key=True)  # ID unik untuk setiap entri dalam posting list
+    termlemma = models.ForeignKey(TermLemmas, on_delete=models.CASCADE)  # ID term yang merujuk ke Tabel Term
+    docdetail = models.ForeignKey(DocDetails, on_delete=models.CASCADE)  # Frekuensi kemunculan term dalam dokumen tertentu
+
+    def __str__(self):
+        return f"{self.termlemma} - {self.docdetail}"
+    
+# Model NER Default
 nlp_default = spacy.load("en_core_web_sm")
 
-# Menggabungkan entitas dari model NER default dan kustom
+# Model NER Custom
+model_path = "kms_kopi/training/model-best"
+if os.path.exists(model_path):
+    nlp_custom = spacy.load(model_path)
+else:
+    # Jika model khusus tidak ditemukan, buat model kosong
+    nlp_custom = spacy.blank("en")  
+
+    train_data_path = 'kms_kopi/training/train_data.json'
+
+    # Cek apakah file JSON ada
+    if os.path.exists(train_data_path):
+        # Buka file JSON yang berisi data train
+        with open(train_data_path, 'r', encoding='utf-8') as f:
+            TRAIN_DATA = json.load(f)
+
+        # Filter anotasi untuk menghapus entri null
+        filtered_annotations = [annotation for annotation in TRAIN_DATA['annotations'] if annotation is not None]
+
+        # Inisialisasi objek DocBin untuk menyimpan dokumen Spacy
+        db = DocBin()
+
+        # Iterasi melalui anotasi yang difilter
+        for text, annot in tqdm(filtered_annotations):
+            # Buat objek dokumen Spacy dari teks
+            doc = nlp_default.make_doc(text)
+            ents = []
+
+            # Iterasi melalui entitas yang diberikan dalam anotasi
+            for start, end, label in annot["entities"]:
+                # Buat objek span untuk entitas
+                span = doc.char_span(start, end, label=label, alignment_mode="contract")
+                if span is None:
+                    print("Melewati entitas")
+                else:
+                    ents.append(span)
+
+            # Atur entitas yang ditemukan dalam dokumen
+            doc.ents = ents
+            # Tambahkan dokumen ke DocBin
+            db.add(doc)
+
+        # Simpan data pelatihan ke disk dalam format Spacy
+        db.to_disk("kms_kopi/training/training_data.spacy")
+
+        # Eksekusi perintah setelah penyimpanan data ke disk
+        init_config_args = "init config kms_kopi/training/config.cfg --lang en --pipeline ner --optimize efficiency"
+        train_args = "train kms_kopi/training/config.cfg --output kms_kopi/training/ --paths.train kms_kopi/training/training_data.spacy --paths.dev kms_kopi/training/training_data.spacy"
+
+        # Jalankan perintah untuk inisialisasi konfigurasi
+        subprocess.run(["python", "-m", "spacy"] + init_config_args.split())
+
+        # Jalankan perintah untuk melatih model
+        subprocess.run(["python", "-m", "spacy"] + train_args.split())
+
 def merge_entities(doc):
     combined_entities = []
     entities_custom = {}
@@ -36,11 +141,11 @@ def merge_entities(doc):
     for ent in nlp_default(doc.text).ents:
         entities_default[(ent.start_char, ent.end_char)] = (ent.label_, ent.text)
 
-    # Entitas dari model NER train
+    # Entitas dari model NER custom
     for ent in nlp_custom(doc.text).ents:
         entities_custom[(ent.start_char, ent.end_char)] = (ent.label_, ent.text)
 
-    # Gabungan entitas
+    # Gabungkan entitas
     combined_entities = entities_custom.copy()
 
     for (start_char, end_char), (label, text) in entities_default.items():
@@ -52,110 +157,20 @@ def merge_entities(doc):
         if not overlap:
             combined_entities[(start_char, end_char)] = (label, text)
 
-    # Buat daftar untuk menyimpan span
+    print(combined_entities)
+    # Create a list to store spans
     spans = []
 
-    # Buat span menggunakan offset karakter langsung
+    # Create spans using character offsets directly
     for (start_char, end_char), (label, text) in combined_entities.items():
         span = doc.char_span(start_char, end_char, label=label)
         if span is None:
-            print(f"Melewati entitas: {text}")
+            print(f"Skipping entity: {text}")
         else:
             spans.append(span)
 
-    # Buat dokumen baru dengan entitas yang digabungkan
+    # Create a new document with the combined entities
     merged_doc = Doc(doc.vocab, words=[token.text for token in doc])
     merged_doc.ents = spans
 
     return merged_doc
-
-# Load model NER custom
-nlp_custom = spacy.load("kms_app/training/model-best")
-
-# Tambahkan sentencizer ke pipeline model NER custom
-if "sentencizer" not in nlp_custom.pipe_names:
-    nlp_custom.add_pipe("sentencizer")
-
-# Path ke file PDF
-directory_path = "kms_app/uploaded_files/"
-
-for filename in os.listdir(directory_path):
-    if filename.endswith(".pdf"):  # Pastikan itu file PDF
-        pdf_path = os.path.join(directory_path, filename)
-        if os.path.exists(pdf_path):
-            # Jika file PDF sudah ada
-            try:
-                # Buka file PDF
-                doc = fitz.open(pdf_path)
-        
-                # Ambil teks dari halaman PDF
-                for page_number in range(doc.page_count):
-                    page = doc[page_number]
-                    text = page.get_text()
-                    text = text.replace('\n', ' ')
-            
-                    document = []
-            
-                    # Bagi teks menjadi kalimat
-                    sentences = text.split('.')
-
-                    # Proses setiap kalimat
-                    for sentence in sentences:
-                        # Gabungkan entitas NER
-                        doc = merge_entities(nlp_custom(sentence))
-                        document.append(doc.text)
-            except Exception as e:
-                pass
-    else: 
-        pass
-
-# Path ke file JSON yang berisi data train
-train_data_path = 'kms_app/training/train_data.json'
-
-# Buka file JSON yang berisi data train
-with open(train_data_path, 'r', encoding='utf-8') as f:
-    TRAIN_DATA = json.load(f)
-
-# Filter anotasi untuk menghapus entri null
-filtered_annotations = [annotation for annotation in TRAIN_DATA['annotations'] if annotation is not None]
-
-# Inisialisasi objek DocBin untuk menyimpan dokumen Spacy
-db = DocBin()
-
-# Iterasi melalui anotasi yang difilter
-for text, annot in tqdm(filtered_annotations):
-    # Buat objek dokumen Spacy dari teks
-    doc = nlp_default.make_doc(text)
-    ents = []
-
-    # Iterasi melalui entitas yang diberikan dalam anotasi
-    for start, end, label in annot["entities"]:
-        # Buat objek span untuk entitas
-        span = doc.char_span(start, end, label=label, alignment_mode="contract")
-        if span is None:
-            print("Melewati entitas")
-        else:
-            ents.append(span)
-
-    # Atur entitas yang ditemukan dalam dokumen
-    doc.ents = ents
-    # Tambahkan dokumen ke DocBin
-    db.add(doc)
-
-# Simpan data pelatihan ke disk dalam format Spacy
-db.to_disk("kms_app/training/training_data.spacy")
-
-# Cek apakah model sudah ada sebelumnya
-model_trained = os.path.exists("kms_app/training/model-best")
-
-# Jika model belum ada, maka jalankan proses training
-if not model_trained:
-    # Eksekusi perintah setelah penyimpanan data ke disk
-    init_config_args = "init config kms_app/training/config.cfg --lang en --pipeline ner --optimize efficiency"
-    train_args = "train kms_app/training/config.cfg --output kms_app/training/ --paths.train kms_app/training/training_data.spacy --paths.dev kms_app/training/training_data.spacy"
-
-    # Jalankan perintah untuk inisialisasi konfigurasi
-    subprocess.run(["python", "-m", "spacy"] + init_config_args.split())
-
-    # Jalankan perintah untuk melatih model
-    subprocess.run(["python", "-m", "spacy"] + train_args.split())
